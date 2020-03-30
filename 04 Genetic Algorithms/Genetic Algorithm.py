7f9dfc738170380bf053a508c7c620c07b71bc02
Jiawei Wang <wangjiawei_med@hotmail.com>
2020-03-30 16:54:54 -0400

    Upload 04 Genetic Algorithm

--- 04 Genetic Algorithms/Genetic Algorithm.py
+++ 04 Genetic Algorithms/Genetic Algorithm.py
@@ -1,15 +1,32 @@
-'''
-1. 初始化10个样本（每个是12位的binary，输出为10个字符串的一维list）                     finished
-2. Print：初代样本为。。。                                                        finished
-3. 以下步骤loop 100次除非提前达到目标：                                             finished
-4. 计算适应度（输入10个样本的list，输出为10个样本的重量和价值的二维list）                 finished
-5. 判断是否达到停止loop的标准（输入为二维list，输出为true/false）                      finished
-6. 如果达到标准，输出最优解，如果没有，进入步骤7
-7. 找到进行繁殖的5个样本（输入为一维list和二维list，输出为符合要求的样本中的前五名）        finished
-8. 对这五个样本进行繁殖（输入为5个样本，输出为10个后代）
-9. 对10个后代进行突变（输入为10个后代，输出为随机突变后的10个后代）
-10. 返回步骤4
-'''
+"""
+COMP 131 AI Assignment 04
+Genetic Algorithm for Backpack Problem
+Author: Jiawei Wang
+Date: Mar 30, 2020
+
+
+Please read README.txt before running this program
+
+
+!!!NOTICE!!!:  (PLEASE READ)
+
+In order to implement as many functions as possible in this algorithm (Fitness Calculation, Crossing, Mutation, etc), I
+enforced every generation to go through all these functions, which in nature do not happen all the time.
+
+So the major consequence of this decision is: Even though sometimes the algorithm gets some prefect samples (with weight
+close to 250 and relative high value), it still enforces these sample to cross and randomly mutate, which can mostly
+damage the genes and lead to a worse generation (that is to say the best sample in offspring generation even has lower
+weight and value compared to parent generation).
+
+Because in every generation I only have 10 samples and select no more than 5 of them (otherwise it is impossible to
+output every generation on the screen and the program consumes too much time to run), so the genes in this small group
+is very unstable, Crossing and Mutation can be disastrous (Similar in Biology, if the size of one species is small,
+then most of the time they extinct after several generations).
+
+So when you finish running this program, sometimes you will get bad results (for example: weight 100, value 8), in order
+to solve this problem, you need to run this program several times and read through the "evolution record"
+"""
+
 
 import random
 
@@ -32,7 +49,7 @@ items = {  # number : [weight, value]
 weight_limit = 250
 
 # max value in last generation (parent generation), initialized as 0 for first generation
-max_last = 0
+max_last = 1
 
 # change of max value between parent and grandparent generation, initialized as inf for first generation
 diff_last = float("inf")
@@ -43,13 +60,17 @@ finish_limit = 2
 # maximum number of samples that can reproduce off-springs
 selected_number = 5
 
+# Number of genes on a sample
+chromosome = len(items)
+
+
 # Randomly initialize 10 samples, each sample is a string consists of 12 random '0's and '1's
 def init():
     init_states = []
     for i in range(10):
         x = random.randint(1, 4095)  # 4095 is 12 '1's in binary
         binary = str(bin(x)[2:])
-        while len(binary) < 12:
+        while len(binary) < chromosome:
             binary = "0"+binary
         init_states.append(binary)
     return init_states  # pack 10 string into a 1-D list
@@ -77,9 +98,8 @@ def is_finished(fitness_list):
     for element in fitness_list:
         if element[1] > max_current:
             max_current = element[1]
-    print("Max value for current generation is: ", max_current)
     diff = max_current - max_last  # change of max value between current generation and parent generation
-    if diff < finish_limit:
+    if 0 <= diff < finish_limit:
         return True
     else:
         diff_last = diff  # record max value difference between current generation and parent generation
@@ -90,44 +110,77 @@ def is_finished(fitness_list):
 # Select the 5 best samples for reproduction
 def selection(chromosome_states, fitness_list):
     legit_sample = []  # store legit samples of current generation
-    selected_list = {}
+    selected_list = []
     for index in range(len(chromosome_states)):
         if fitness_list[index][0] <= weight_limit:
             # [value, weight, genes]
             legit_sample.append([fitness_list[index][1], fitness_list[index][0], chromosome_states[index]])
     if len(legit_sample) <= selected_number: # if legit samples are less than or equal to 5, all are selected
         for i in range(len(legit_sample)):
-            selected_list[legit_sample[i][2]] = [legit_sample[i][1], legit_sample[i][0]]
+            selected_list.append(str(legit_sample[i][2]))
     else:  # if legit samples are more than 5, we choose the first 5 with highest value
         selection = sorted(legit_sample, key=lambda x: x[0])
         for i in range(selected_number):
-            selected_list[selection[i][2]] = [selection[i][1], selection[i][0]]
-    return selected_list  # genes : [weight, value] (caution: selected list may not always contain 5 selected samples)
+            selected_list.append(str(selection[i][2]))
+    return selected_list  # 1-D list of samples (caution: selected list may contain less than 5 samples)
 
 
 # Reproduce next generation
 def reproduce(selected_list):
+    offspring = []
+    while len(offspring) < 10:
+        mom, dad = random.sample(selected_list, 2)  # randomly select 2 different samples from the strong samples
+        index = random.randint(1, chromosome-2)  # [1, 10], which means we keep at least 1 gene from each parent
+        baby = mom[:index] + dad[index:]
+        offspring.append(baby)
+    return offspring  # 1-D list with 10 new next-generation samples
 
+
+# Randomly mutate offsprings
+def mutation(offspring):
+    chromosome_states = []
+    for sample in offspring:
+        mutate = random.choice([True, False])
+        if mutate:
+            index = random.randint(0, chromosome - 1)
+            temp = list(sample)
+            temp[index] = str(abs(1-int(temp[index])))  # if the original gene is 1, change to 0 (if 0, change to 1)
+            sample = "".join(temp)
+        chromosome_states.append(sample)
+    return chromosome_states
 
 
 # Let's run this program
+print("---------------------------------------------------------------------")
 print("Nature Selection Pro 3000 started!")
 chromosome_states = init()  # get 10 random samples
 print("The 10 samples of first generation are: ")
 print(chromosome_states)
+print("---------------------------------------------------------------------")
+
 generation_loop = 100
 while generation_loop > 0:  # reproduce 100 generations until the requirement is met
     generation_loop -= 1
+    print("Generation No.", 100-generation_loop)
     fitness_list = fitness(chromosome_states)
-    print("Fitness of current generation samples are [weight, value]:")
+    print("Fitness [weight, value] of current generation samples are:")
     print(fitness_list)
     if is_finished(fitness_list):  # if samples of current generation meet the requirement
         print("This generation meets the requirement, program terminated")
+        print("---------------------------------------------------------------------")
         break
     print("This generation does not meet the requirement, reproduction continues")
+    print("---------------------------------------------------------------------")
     selected_list = selection(chromosome_states, fitness_list)  # choose which samples can reproduce
-    next_generation = reproduce(selected_list)
+    offspring = reproduce(selected_list)  # reproduce next generation
+    chromosome_states = mutation(offspring)  # mutation within next generation and repeat whole process
 
+selected_list = selection(chromosome_states, fitness_list)
+print("The fittest samples for the last generation are:")
+print(selected_list)
+result = fitness(selected_list)
+print("Their weight and value are:")
+print(result)
 
 
 
